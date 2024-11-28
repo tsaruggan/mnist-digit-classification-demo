4d9ab0cc96dfc0534d862db0893086d6359cc995
tsaruggan <tsaruggan@gmail.com>
2024-11-27 19:31:22 -0500

    import syntax

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const axios = require('axios');
+import express from 'express';
+import axios from 'axios';
+
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-
 app.use(express.static('public'));
 
 app.post('/predict', async (req, res) => {
